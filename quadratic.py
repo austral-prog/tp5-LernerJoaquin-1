c465e4561a15583be63fcde010688b2fd504678a
LernerJoaquin <lernerjoaquin@gmail.com>
2025-09-12 18:36:29 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,28 +1,22 @@
 import math
-def roots(a, b, c):
-    discriminante = b**2 - 4*a*c
-    if discriminante > 0:
-        r1 = ((-b + math.sqrt(discriminante)) / (2*a))
-        r2 = ((-b - math.sqrt(discriminante)) / (2*a))
-        print(f"({r1}, {r2})")
-        return
-    elif discriminante == 0:
-        r3 = -b / (2*a)
-        print(f"({r3})")
-    else:
-        print("( )")
-        return
-            
-    
-def value_y(a, b, c, x):
-    ecuacion = ((a*(x**2)) + b*x + c )
-    print(ecuacion)
 
+def roots(a, b, c):
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        r1 = (-b + math.sqrt(discriminant)) / (2*a)
+        r2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return f"({r1}, {r2})"
+    elif discriminant == 0:
+        r = -b / (2*a)
+        return f"({r})"
+    else:
+        return "( )"
+
+def value_y(a, b, c, x):
+    return a * x**2 + b * x + c
 
 def to_string(a, b, c):
-    print(f"f(x) = {a} * X^2 + {b} * X + {c}") 
-    
-
+    return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
 def derivation(a, b, c):
-    print(f"f'(x) = {2*a}x + {b}")
+    return f"f'(x) = {2*a}x + {b}"
